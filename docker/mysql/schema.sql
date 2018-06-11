9d4c29380b5f607a66ff50fa413a1bf4b35dc459
Joy  Ndjama <joyjedid@gmail.com>
2018-06-11 15:27:18 +0200

    init

---
+++ docker/mysql/schema.sql
@@ -0,0 +1,9 @@
+create database dbapp;
+use dbapp;
+
+CREATE TABLE if not exists `todo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `description` text NOT NULL,
+  `status` tinyint(4) NOT NULL Default 0 COMMENT '0 for not completed, 1 for completed, 2 for hidden',
+  `date_time` datetime NULL
+);
---
+++ docker/mysql/todonit.sql
@@ -0,0 +1,3 @@
+use dbapp;
+INSERT INTO `todo` (`id`, `description`, `status`, `date_time`) VALUES
+(22, 'Allow you to prioritize your active tasks', 0, '0000-00-00 00:00:00');
